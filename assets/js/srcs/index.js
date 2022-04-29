7e474a09633c5cfe114539f42bfec14c2ca5a246
robmccormack89 <robertmccormack147@gmail.com>
2022-04-29 17:18:18 +0100

    Macy.js added for masonry grid fix

--- assets/js/srcs/index.js
+++ assets/js/srcs/index.js
@@ -1,6 +1,8 @@
 import UIkit from 'uikit'; // import uikit
 import Icons from 'uikit/dist/js/uikit-icons'; // import uikit icons
-
 UIkit.use(Icons); // use the Icon plugin
-
 window.UIkit = UIkit; // Make uikit available in window for inline scripts
+
+// import Macy from 'macy';
+import * as MasonryGrid from 'macy'; // we use macy to fix bug in uk-grid: masonry
+window.MasonryGrid = MasonryGrid
