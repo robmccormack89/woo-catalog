0a1a077e9e61d36d5877ff2cd49030b743dbdb38
Robert McCormack <robertmccormack147@gmail.com>
2022-06-21 21:42:22 +0100

    ...

--- inc/Theme.php
+++ inc/Theme.php
@@ -46,10 +46,10 @@ class Theme extends Timber {
     *
     */
     add_action('after_setup_theme', array($this, 'theme_supports'));
-		add_filter('timber/context', array($this, 'add_to_context'));
-		add_filter('timber/twig', array($this, 'add_to_twig'));
-		add_action('init', array($this, 'register_post_types'));
-		add_action('init', array($this, 'register_taxonomies'));
+    add_filter('timber/context', array($this, 'add_to_context'));
+    add_filter('timber/twig', array($this, 'add_to_twig'));
+    add_action('init', array($this, 'register_post_types'));
+    add_action('init', array($this, 'register_taxonomies'));
     add_action('init', array($this, 'register_widget_areas'));
     add_action('init', array($this, 'register_navigation_menus'));
     add_action('enqueue_block_assets', array($this, 'theme_enqueue_assets')); // use 'theme_enqueue_assets' for frontend-only
